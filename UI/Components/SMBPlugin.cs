5d1401796b43e2ae8e850992a7d182bf90098a75
Sergey Papushin <spapushin@gmail.com>
2016-09-18 14:29:17 -0500

    Use a more precise frame rule

--- UI/Components/SMBPlugin.cs
+++ UI/Components/SMBPlugin.cs
@@ -9,152 +9,153 @@ using System.Xml;
 
 namespace LiveSplit.UI.Components
 {
-	public class SMBPlugin : IComponent, IDisposable
-	{
-		private TimeStamp PauseTime;
+    public class SMBPlugin : IComponent, IDisposable
+    {
+        private TimeStamp PauseTime;
+        private const double frameRule = 21 / 60.098813;
 
-		public float PaddingTop
-		{
-			get
-			{
-				return 0f;
-			}
-		}
+        public float PaddingTop
+        {
+            get
+            {
+                return 0f;
+            }
+        }
 
-		public float PaddingLeft
-		{
-			get
-			{
-				return 0f;
-			}
-		}
+        public float PaddingLeft
+        {
+            get
+            {
+                return 0f;
+            }
+        }
 
-		public float PaddingBottom
-		{
-			get
-			{
-				return 0f;
-			}
-		}
+        public float PaddingBottom
+        {
+            get
+            {
+                return 0f;
+            }
+        }
 
-		public float PaddingRight
-		{
-			get
-			{
-				return 0f;
-			}
-		}
+        public float PaddingRight
+        {
+            get
+            {
+                return 0f;
+            }
+        }
 
-		protected ITimerModel Model
-		{
-			get;
-			set;
-		}
+        protected ITimerModel Model
+        {
+            get;
+            set;
+        }
 
-		protected LiveSplitState CurrentState
-		{
-			get;
-			set;
-		}
+        protected LiveSplitState CurrentState
+        {
+            get;
+            set;
+        }
 
-		public float VerticalHeight
-		{
-			get
-			{
-				return 0f;
-			}
-		}
+        public float VerticalHeight
+        {
+            get
+            {
+                return 0f;
+            }
+        }
 
-		public float MinimumWidth
-		{
-			get
-			{
-				return 0f;
-			}
-		}
+        public float MinimumWidth
+        {
+            get
+            {
+                return 0f;
+            }
+        }
 
-		public float HorizontalWidth
-		{
-			get
-			{
-				return 0f;
-			}
-		}
+        public float HorizontalWidth
+        {
+            get
+            {
+                return 0f;
+            }
+        }
 
-		public float MinimumHeight
-		{
-			get
-			{
-				return 0f;
-			}
-		}
+        public float MinimumHeight
+        {
+            get
+            {
+                return 0f;
+            }
+        }
 
-		public string ComponentName
-		{
-			get
-			{
-				return "Super Mario Bros. Plugin";
-			}
-		}
+        public string ComponentName
+        {
+            get
+            {
+                return "Super Mario Bros. Plugin";
+            }
+        }
 
-		public IDictionary<string, Action> ContextMenuControls
-		{
-			get
-			{
-				return null;
-			}
-		}
+        public IDictionary<string, Action> ContextMenuControls
+        {
+            get
+            {
+                return null;
+            }
+        }
 
-		public SMBPlugin(LiveSplitState state)
-		{
+        public SMBPlugin(LiveSplitState state)
+        {
             PauseTime = TimeStamp.Now;
-			var timerModel = new TimerModel();
+            var timerModel = new TimerModel();
             timerModel.CurrentState = state;
             state.OnSplit += State_OnSplit;
             Model = timerModel;
             CurrentState = state;
-		}
+        }
 
         private void State_OnSplit(object sender, EventArgs e)
         {
             if (CurrentState.CurrentPhase == TimerPhase.Running)
             {
                 var lastSplit = CurrentState.Run[CurrentState.CurrentSplitIndex - 1].SplitTime;
-                var fixedTime = Math.Round(lastSplit[TimingMethod.RealTime].Value.TotalSeconds * (1 / 0.35)) * 0.35;
+                var fixedTime = Math.Round(lastSplit[TimingMethod.RealTime].Value.TotalSeconds * (1 / frameRule)) * frameRule;
                 lastSplit[TimingMethod.RealTime] = TimeSpan.FromSeconds(fixedTime);
                 CurrentState.Run[CurrentState.CurrentSplitIndex - 1].SplitTime = lastSplit;
             }
         }
 
         public void DrawVertical(Graphics g, LiveSplitState state, float width, Region clipRegion)
-		{
-		}
+        {
+        }
 
-		public void DrawHorizontal(Graphics g, LiveSplitState state, float height, Region clipRegion)
-		{
-		}
+        public void DrawHorizontal(Graphics g, LiveSplitState state, float height, Region clipRegion)
+        {
+        }
 
-		public Control GetSettingsControl(LayoutMode mode)
-		{
+        public Control GetSettingsControl(LayoutMode mode)
+        {
             return null;
-		}
+        }
 
-		public void SetSettings(XmlNode settings)
-		{
-		}
+        public void SetSettings(XmlNode settings)
+        {
+        }
 
-		public XmlNode GetSettings(XmlDocument document)
-		{
+        public XmlNode GetSettings(XmlDocument document)
+        {
             return document.CreateElement("SMBPluginSettings");
-		}
+        }
 
-		public void Update(IInvalidator invalidator, LiveSplitState state, float width, float height, LayoutMode mode)
-		{
-		}
+        public void Update(IInvalidator invalidator, LiveSplitState state, float width, float height, LayoutMode mode)
+        {
+        }
 
-		public void Dispose()
-		{
+        public void Dispose()
+        {
             CurrentState.OnSplit -= State_OnSplit;
-		}
-	}
+        }
+    }
 }
--- UI/Components/SMBPluginFactory.cs
+++ UI/Components/SMBPluginFactory.cs
@@ -7,67 +7,67 @@ using UpdateManager;
 
 namespace LiveSplit.UI.Components
 {
-	public class SMBPluginFactory : IComponentFactory, IUpdateable
-	{
-		public string ComponentName
-		{
-			get
-			{
-				return "Super Mario Bros. Plugin";
-			}
-		}
+    public class SMBPluginFactory : IComponentFactory, IUpdateable
+    {
+        public string ComponentName
+        {
+            get
+            {
+                return "Super Mario Bros. Plugin";
+            }
+        }
 
-		public string Description
-		{
-			get
-			{
-				return "Rounds splits to the nearest frame rule.";
-			}
-		}
+        public string Description
+        {
+            get
+            {
+                return "Rounds splits to the nearest frame rule.";
+            }
+        }
 
-		public ComponentCategory Category
-		{
-			get
-			{
-				return ComponentCategory.Control;
-			}
-		}
+        public ComponentCategory Category
+        {
+            get
+            {
+                return ComponentCategory.Control;
+            }
+        }
 
-		public string UpdateName
-		{
-			get
-			{
-				return ComponentName;
-			}
-		}
+        public string UpdateName
+        {
+            get
+            {
+                return ComponentName;
+            }
+        }
 
-		public string XMLURL
-		{
-			get
-			{
-				return "http://livesplit.org/update/Components/noupdates.xml";
-			}
-		}
+        public string XMLURL
+        {
+            get
+            {
+                return "http://livesplit.org/update/Components/noupdates.xml";
+            }
+        }
 
-		public string UpdateURL
-		{
-			get
-			{
-				return "http://livesplit.org/update/";
-			}
-		}
+        public string UpdateURL
+        {
+            get
+            {
+                return "http://livesplit.org/update/";
+            }
+        }
 
-		public Version Version
-		{
-			get
-			{
-				return Version.Parse("1.0");
-			}
-		}
+        public Version Version
+        {
+            get
+            {
+                return Version.Parse("1.0");
+            }
+        }
 
-		public IComponent Create(LiveSplitState state)
-		{
-			return new SMBPlugin(state);
-		}
-	}
+        public IComponent Create(LiveSplitState state)
+        {
+            return new SMBPlugin(state);
+        }
+    }
 }
